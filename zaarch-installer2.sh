d35203d50ac58d772ac2734ec0acdd9a7fa3a116
Mohamed8Zanaty <156523241+Mohamed8Zanaty@users.noreply.github.com>
2025-01-17 22:40:09 +0200

    Update zaarch-installer2.sh

--- zaarch-installer2.sh
+++ zaarch-installer2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-sed -i "#en_US.UTF-8 UTF-8" "en_US.UTF-8 UTF-8" /etc/locale.gen
+sudo sed -i 's/^en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
