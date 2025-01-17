335a18be2f82fdc0258ea39c5ef4771b75110d91
Mohamed8Zanaty <156523241+Mohamed8Zanaty@users.noreply.github.com>
2025-01-17 21:07:59 +0200

    Add files via upload

--- zaarch-installer.sh
+++ zaarch-installer.sh
@@ -1,14 +1,107 @@
 #!/bin/bash
 
-echo "HI TO ZA-ARCH-INSTALLER"
+echo "            HI TO ZA-ARCH-INSTALLER"
 echo "THIS SCRIPT TO INSTALL BASE ARCH WITH OUT WM"
-echo "First Enter Your EFI Partition's Path: "
 while true; do
-  read efiPath
+  read -p "Enter Your EFI Partition's Path: " efiPath
   if [ -e "$efiPath" ]; then
     break
   fi
   echo "Partition Not Fount, Try Again: "
 done
 
-echo $efiPath
+while true; do
+  read -p "Enter Root Partition's Path: " rootPath
+  if [ -e "$rootPath" ]; then
+    break
+  fi
+  echo "Partition Not Fount, Try Again"
+done
+
+while true; do
+  read -p "Do You Want To Make Swap Filr [y or n]: " swapAns
+  if [ "$swapAns" = "y"]; then
+    while true; do
+      read -p "Enter Swap Partition's Path: " swapPath
+      if [ -e "$rootPath" ]; then
+        break
+      fi
+      echo "Partition Not Fount, Try Again"
+    done
+    break
+  fi
+  if [ "$swapAns" = "n"]; then
+    break;
+  fi
+  echo "Partition Not Fount, Try Again"
+done
+
+mkfs.fat -F32 $efiPath
+mkfs.ext4 $rootPath
+if [ "$swapAns" = "y" ]; then
+  mkswap $swapPath
+  swapon $swapPath
+fi
+
+mkdir -p /mnt/boot/efi
+
+mount $rootPath /mnt
+mount $efiPath /mnt/boot/efi
+
+echo "Base System Packages will be installed -> [ base, linux, linux-firmware, sudo, nano, git ]"
+pacstrap /mnt base linux linux-firmware sudo nano git
+
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt /bin/bash
+sed -i "#en_US.UTF-8 UTF-8" "en_US.UTF-8 UTF-8" /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# time zone
+ln -sf /usr/share/zoneinfo/Africa/Cairo /etc/localtime
+hwclock --systohc
+
+echo "arch" > /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1  localhost" >> /etc/hosts
+echo "127.0.1.1 arch" >> /etc/hosts
+
+echo "Create Root Password..."
+passwd
+
+echo "Installing NetworkManager..."
+pacman -S networkmanager
+systemctl enable NetworkManager
+
+echo "Insatll BootLoader..."
+pacman -S grub efibootmgr
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Install X Window System..."
+pacman -S xorg xorg-xinit xorg-server
+
+echo "Creating New User..."
+while true; do
+  read -p "Enter Username: " username
+  if [[ "$username" =~ ^[a-z_][a-z0-9_-]{0,31}$ ]]
+    break
+  fi
+  echo "Invalid Username, Try Again"
+done
+useradd -m -G wheel -s /bin/bash $username
+passwd $username
+
+export EDITOR=nano
+file="/etc/sudoers"
+searchLine="# %wheel ALL=(ALL) ALL"
+replaceLine="%wheel ALL=(ALL) ALL"
+if grep -q "$searchLine" "$file"; then
+  sudo sed -i $searchLine $replaceLine $file
+fi
+exit
+reboot
+
+
